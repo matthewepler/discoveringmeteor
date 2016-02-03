2c2d70dbcb10b03b04e7c3e1c9b6e245c1812dea
Matthew Epler <matthewepler@gmail.com>
2016-02-03 18:45:55 -0500

    up to reactivity

--- microscope2/lib/router.js
+++ microscope2/lib/router.js
@@ -2,7 +2,14 @@
 Router.configure({
 	layoutTemplate: 'layout', // matches filename 
 	loadingTemplate: 'loading',
+	notFoundTemplate: 'notFound',
 	waitOn: function() { return Meteor.subscribe('posts'); } // will subscribe to this for every page
 });
 
 Router.route('/', {name: 'postsList'}); // matches filename
+Router.route('/posts/:_id', {
+	name: 'postPage',
+	data: function() { return Posts.findOne(this.params._id) }
+});
+
+Router.onBeforeAction('dataNotFound', {only: 'postPage'}); // shows notFound template
